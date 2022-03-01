047407d74d8dbe186867fa6e6cf0770d7eba80ce
Donaldo Solano <solanonaranj@gmail.com>
2022-02-28 23:46:47 -0500

    no message

--- Source/Undegard/Private/Undegard_Door.cpp
+++ Source/Undegard/Private/Undegard_Door.cpp
@@ -2,6 +2,9 @@
 
 
 #include "Undegard_Door.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/BoxComponent.h"
+#include "Undegard_Character.h"
 
 // Sets default values
 AUndegard_Door::AUndegard_Door()
@@ -11,6 +14,8 @@ AUndegard_Door::AUndegard_Door()
 
 	openAngle = -90.0f;
 
+	DoorTag = "KeyA";
+
 	CustomRootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("CustomRoot"));
 	RootComponent = CustomRootComponent;
 
@@ -19,13 +24,35 @@ AUndegard_Door::AUndegard_Door()
 
 	DoorComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Door"));
 	DoorComponent->SetupAttachment(CustomRootComponent);
+
+	KeyZoneColliderComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("KeyZoneCollider"));
+	KeyZoneColliderComponent->SetupAttachment(CustomRootComponent);
+
+	KeyZoneColliderComponent->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	KeyZoneColliderComponent->SetCollisionResponseToAllChannels(ECR_Ignore);
+	KeyZoneColliderComponent->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
 }
 
 // Called when the game starts or when spawned
 void AUndegard_Door::BeginPlay()
 {
 	Super::BeginPlay();
-	OpenDoor();
+	KeyZoneColliderComponent->OnComponentBeginOverlap.AddDynamic(this, &AUndegard_Door::CheckKeyFromPlayer);
+}
+
+void AUndegard_Door::CheckKeyFromPlayer(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	if (IsValid(OtherActor))
+	{
+		AUndegard_Character* OverlappedCharacter = Cast<AUndegard_Character>(OtherActor);
+		if (IsValid(OverlappedCharacter))
+		{
+			if (OverlappedCharacter->HasKey(DoorTag))
+			{
+				OpenDoor();
+			}
+		}
+	}
 }
 
 // Called every frame
