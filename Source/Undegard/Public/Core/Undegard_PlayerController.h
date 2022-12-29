d979dc20e90d3fb2a71fe460629160c09ad6665d
Donaldo Solano <solanonaranj@gmail.com>
2022-12-28 19:57:37 -0500

    Created player controller class to be able to create different blueprint childs so player controller from the UI can have different properties than the one from gameplay.

---
+++ Source/Undegard/Public/Core/Undegard_PlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Undegard_PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNDEGARD_API AUndegard_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
