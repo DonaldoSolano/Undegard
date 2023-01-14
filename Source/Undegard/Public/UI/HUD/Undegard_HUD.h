d7eb41cdc8da2366df0753459510a060be5e1643
Donaldo Solano <solanonaranj@gmail.com>
2023-01-14 14:18:16 -0500

    HUD classes to handle all character data visualization.

---
+++ Source/Undegard/Public/UI/HUD/Undegard_HUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Undegard_HUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNDEGARD_API UUndegard_HUD : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Undegard/Public/UI/HUD/Undegard_HUDSight.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Undegard_HUDSight.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNDEGARD_API UUndegard_HUDSight : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
